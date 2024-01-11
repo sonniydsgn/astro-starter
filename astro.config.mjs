9a04643fdd6774b30c9d106e45f1c6cb33048ddc
Alexey Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2024-01-11 17:40:27 +0500

    поправил работу astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config'
-// import compress from 'astro-compress'
+import compress from 'astro-compress'
 import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+	compressHTML: false,
 	integrations: [
 		icon(),
-		// compress({
-		// 	CSS: false,
-		// }),
+		compress({
+			CSS: false,
+		}),
 	],
 })
